3241b740e87e37b8a84d34c3d65b927578b77ec8
dodadidi <61885300+dodadidi@users.noreply.github.com>
2020-12-28 20:07:24 +0200

    fix filter by stops

--- controllers/flight.ctrl.js
+++ controllers/flight.ctrl.js
@@ -2,16 +2,34 @@ const flight = require('../models/flight');
 
 exports.flightDbController = {
     getFlights(req,res){
-        flight.find({})
+        const keys = Object.keys(req.query);
+        const findFlights = flight.find({});
+        for(let i = 0 ; i < keys.length; i++){
+            // if(keys[i] == 'departure_date') {
+            //     findFlights.find({departure_date:{$gte: req.query.departure_date}});
+            // }
+            if(keys[i] == 'departure_city') {
+                findFlights.find({departure_city:req.query.departure_city});
+            }
+            else if(keys[i] == 'landing_city') {
+                findFlights.find({landing_city:req.query.landing_city});
+            }
+            else if(keys[i] == 'stops'){
+                findFlights.find({stops: {$lte: req.query.stops}});
+            }
+            else{res.status(404).send("Error: wrong key");}
+        }
+        findFlights
         .then(docs => { res.json(docs)})
-        .catch(err => console.log(`Error getting the data from db: ${err}`));
+        .catch(err => console.log(`Error getting the data from db: ${err}`));  
     },
     addFlight(req,res){
         const newFlight = new flight({
             //check, validations
-            "_id": req.body.id,
+            "id": req.body.id,
             "flight_number": req.body.flight_number,
             "departure_date": req.body.departure_date,
+            // "departure_time": req.body.departure_time,
             "departure_city": req.body.departure_city,
             "landing_city":  req.body.landing_city,
             "company_name":  req.body.company_name,
@@ -29,21 +47,27 @@ exports.flightDbController = {
         }
     },
     getFlight(req,res) {
-        const id = req.params.id;
-        flight.findOne({id: id})
+        const flight_number = req.params.flight_number;
+        flight.findOne({flight_number:flight_number})
         .then(docs => { res.json(docs)})
         .catch(err => console.log(`Error getting the data from db: ${err}`));
     },
     updateFlight(req,res){
-      flight.updateMany({id:req.params.id},{first_name:req.body.first_name,last_name:req.body.last_name,email:req.body.email,gender:req.body.gender,color:req.body.color,job:req.body.job})
+      flight.updateOne({flight_number:req.params.flight_number},{departure_date:req.body.departure_date,landing_city:req.body.landing_city,company_name:req.body.company_name,price:req.body.price})
       .then(docs => { res.json(docs)})
       .catch(err => console.log(`Error updating flight from db: ${err}`));  
     },
+    // updateFlight(req,res){
+    //     flight.updateOne({id:req.params.id},{
+    //         $set: {...req.body}
+    //     })
+    //     .then(docs => { res.json(docs)})
+    //     .catch(err => console.log(`Error updating flight from db: ${err}`));
+    //   },
   deleteFlight(req,res){
-    //flight.deleteMany({id:{$in: [1,3,5,7,9]}})
-    flight.deleteOne({id:req.params.id})
+    flight.deleteOne({flight_number:req.params.flight_number})
     .then(docs => { res.json(docs)})
-      .catch(err => console.log(`Error deleting flight from db: ${err}`));  
+    .catch(err => console.log(`Error deleting flight from db: ${err}`));  
   },
 
 }
