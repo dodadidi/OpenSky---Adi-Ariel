11239e3c9b93c4ff40de8c569f4b52d75387394a
Ariel Hadad <48453004+arieell25@users.noreply.github.com>
2020-12-29 20:15:49 +0200

    changes

--- models/flight.js
+++ models/flight.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 const { Schema, model } = require('mongoose');
 
 const flightSchema = new Schema({
-    //id: { type: Number, required: true },
     flight_number: { type: Number, required: true },
     departure_date: { type: String, required: true },
     time: { type: String, required: true },
@@ -25,31 +23,3 @@ const flightSchema = new Schema({
 const Flight = model('Flight', flightSchema);
 
 module.exports = Flight;
-=======
-const { Schema, model } = require('mongoose');
-
-const flightSchema = new Schema({
-    id: { type: Number, required: true },
-    flight_number: { type: Number, required: true },
-    departure_date: { type: String, required: true },
-    time: { type: String, required: true },
-    departure_city: { type: String, required: true },
-    landing_city: { type: String, required: true },
-    company_name: { type: String, required: true },
-    //TODO: change to number
-    price: { type: String, required: true },
-    stops: { type: Number, required: true },
-    surpriseMe: { type: Boolean, required: true },
-    buyer_id: { type: Number },
-    new_price: {type: Number}
-}, { collection: 'Flights', strict: false });
-/* Setters */
-
-/* Validate() */
-
-/* Middleware: pre-save */
-
-const Flight = model('Flight', flightSchema);
-
-module.exports = Flight;
->>>>>>> 013267fbfae8b823b4831998e9eea025fcc19034
