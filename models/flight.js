21c96c258cc2bcafd3efcae0226ce729c7f8bceb
dodadidi <61885300+dodadidi@users.noreply.github.com>
2020-12-29 15:20:51 +0200

    operations on flight table

--- models/flight.js
+++ models/flight.js
@@ -4,9 +4,11 @@ const flightSchema = new Schema({
     id: { type: Number, required: true },
     flight_number: { type: Number, required: true },
     departure_date: { type: String, required: true },
+    time: { type: String, required: true },
     departure_city: { type: String, required: true },
     landing_city: { type: String, required: true },
     company_name: { type: String, required: true },
+    //TODO: change to number
     price: { type: String, required: true },
     stops: { type: Number, required: true },
     surpriseMe: { type: Boolean, required: true },
