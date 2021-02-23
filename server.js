f79087175ff328a7ec9e6d6506bd0f28d270f018
Adi Yaari <adiyaari01@gmail.com>
2021-02-23 09:13:47 +0200

    cors

--- server.js
+++ server.js
@@ -37,7 +37,8 @@ app.use(passport.session());
 
 // app.use(cors())
 app.use(cors({
-  origin: ["https://pedantic-goldwasser-57a322.netlify.app","https://pedantic-goldwasser-57a322.netlify.app/#/","http://localhost:3000"],
+  origin: ["https://pedantic-goldwasser-57a322.netlify.app"],
+  // origin: ["https://pedantic-goldwasser-57a322.netlify.app","https://pedantic-goldwasser-57a322.netlify.app/#/","http://localhost:3000"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   credentials: true, //
   preflightContinue: true
