a68c6dff574b9bbd16a322cb259879b821c5056f
Adi Yaari <adiyaari01@gmail.com>
2021-02-23 11:05:14 +0200

    cors

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ const authCheck = (req, res, next) => {
   });
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://pedantic-goldwasser-57a322.netlify.app');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.set('Content-Type', 'application/json');
     next();
